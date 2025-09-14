ba11d7bf8656b52f2efa151710777cca733a12f9
CentreMetre <luckyminer01@gmail.com>
2025-09-14 10:13:10 +0100

    Add react project and date range search component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DateTimeRangeSearch from './Components/DateTimeRangeSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <DateTimeRangeSearch />
+    </div>
+  );
+}
+
+export default App;
